de49d8cd2ece934ca1adffed7bc8d690400112b5
Иван <foxneversleep@gmail.com>
2025-06-13 15:56:14 +0300

    Update Ballpit.js

--- ballpit/Ballpit.js
+++ ballpit/Ballpit.js
@@ -1,1 +1,21 @@
+import { createScene } from './renderer.js';
+import { createPhysics } from './physics.js';
+
+let raycaster, camera, scene, renderer, balls;
+
+export function createBallpit(canvas, config = {}) {
+  ({ scene, camera, renderer, raycaster } = createScene(canvas));
+  balls = createPhysics(config);
+
+  scene.add(balls.mesh);
+
+  renderer.setAnimationLoop((time) => {
+    balls.update();
+    renderer.render(scene, camera);
+  });
+
+  return {
+    tryClick: (event) => balls.handleClick(event, camera, raycaster),
+  };
+}
 
