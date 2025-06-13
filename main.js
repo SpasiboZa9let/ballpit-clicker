c4eec843f6a35d507dca2d18317c35d7ef761eff
Иван <foxneversleep@gmail.com>
2025-06-13 17:07:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import Ballpit from './ballpit/Ballpit.js';
 const canvas = document.getElementById('canvas');
 
 Ballpit(canvas, {
-  count: 200,
-  gravity: 0.6,
-  friction: 0.9,
-  wallBounce: 0.95,
-  followCursor: true,
+  count: 100,
+  gravity: 0.05,       // мягкая гравитация
+  friction: 0.9,       // медленное замедление
+  wallBounce: 0.75,    // энергия теряется при отскоке
+  followCursor: false, // отключаем мышь на первом этапе
 });
