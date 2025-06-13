932a30af94f67aa7c6755b5ba19b16b45d0c342e
Иван <foxneversleep@gmail.com>
2025-06-13 16:28:10 +0300

    Update clickHandler.js

--- ballpit/physics/clickHandler.js
+++ ballpit/physics/clickHandler.js
@@ -1,1 +1,27 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.152.2';
 
+export function createClickHandler(instancedMesh, velocities) {
+  return function handleClick(event, camera, raycaster) {
+    const rect = raycaster.domElement?.getBoundingClientRect?.() || {
+      left: 0, top: 0, width: window.innerWidth, height: window.innerHeight
+    };
+
+    const mouse = {
+      x: ((event.clientX - rect.left) / rect.width) * 2 - 1,
+      y: -((event.clientY - rect.top) / rect.height) * 2 + 1
+    };
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(instancedMesh);
+
+    if (intersects.length > 0) {
+      const index = intersects[0].instanceId;
+      if (index !== undefined) {
+        velocities[index].add(new THREE.Vector3(0, 1, 0));
+        return true;
+      }
+    }
+
+    return false;
+  };
+}
